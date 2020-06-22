6136e333835cc8a25895d31bb023070919296edd
Abhee Hudani <hudaniabhee@gmail.com>
2020-06-22 18:00:53 +0530

    Spider -> TakeFirst Added

--- Parse/Scrapy/ItemLoaderSpider/ItemLoaderSpider/spiders/company_details_itemloader.py
+++ Parse/Scrapy/ItemLoaderSpider/ItemLoaderSpider/spiders/company_details_itemloader.py
@@ -2,6 +2,7 @@ import scrapy
 
 from ItemLoaderSpider.items import finance_details
 from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst
 
 class CompanyDetailsItemLoaderSpider(scrapy.Item):
     name= "company_details_itemloader"
@@ -13,6 +14,9 @@ class CompanyDetailsItemLoaderSpider(scrapy.Item):
 
         for company in company_results:
             details_loader= ItemLoader(item = CompanyDetailsItem(), selector= company)
+
+            details_loader.default_output_processor = TakeFirst()
+
             details_loader.add_xpath('company_name','td[2]/text()')
             details_loader.add_xpath('company_symbol_link','td[1]/a/@href')
             details_loader.add_xpath('company_price_intraday', 'td[3]/span/text()')
